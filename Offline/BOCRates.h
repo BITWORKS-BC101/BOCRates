558a198d15f2618af1c4f8a5c7b4f232f81b5531
Xuhui.He <130081507@qq.com>
2020-03-31 01:04:57 +0800

    Update BOCRates.h

--- Offline/BOCRates.h
+++ Offline/BOCRates.h
@@ -6,12 +6,12 @@ struct EXCHANGE_RATE
 	double BuyingRate = 0;
 	double CashBuyingRate = 0;
 	double SellingRate = 0;
-	double CashSellingRate =0;
+	double CashSellingRate = 0;
 	double MiddleRate = 0;
 };
 
 typedef struct EXCHANGE_RATE ExchangeRate;
 double ConvertCurrency(int real, const char* from, const char* to, double amount);
-int GetBOCRatesByCode(const char* code, char* name, char* updateTime, double* rates);
+int GetBOCRatesByCode(const char* code, char* name, char* publishTime, double* rates);
 int ReadBOCRatesByCode(const char* code, ExchangeRate* results);
 int ReadAllBOCRates(ExchangeRate** result);
